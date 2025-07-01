e124f99286a6e293445b1302fd4d1ea0eefbf5d1
CharlesReed1311 <chaitanyaarora345@gmail.com>
2025-07-01 15:28:52 +0530

    added graphs

--- backend.py
+++ backend.py
@@ -2,20 +2,14 @@ import requests
 
 API_KEY = "18c7307e66797ee57ecbdc83901c9013"
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data =  response.json()
     filtered_data = data["list"]
     no_of_values = 8*forecast_days
     filtered_data = filtered_data[:no_of_values]
-    
-    if kind == "Temperature":
-        filtered_data = [temp["main"]["temp"] for temp in filtered_data]
-    if kind == "Sky":
-        filtered_data = [sky["weather"][0]["main"] for sky in filtered_data]
-    
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data("Tokyo",3,"Sky"))
+    print(get_data("Tokyo",3))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather forecast")
 
@@ -9,3 +10,19 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    try:
+        filtered_data = get_data(place, days)
+
+        if option == "Temperature":
+            temps = [temp["main"]["temp"]/10 for temp in filtered_data]/10
+            dates = [date["dt_txt"] for date in filtered_data]
+            figure = px.line(x=dates, y=temps, labels={"x":"Date","y":"Temperature(C)"})
+            st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear":r"images\clear.png", "Clouds":r"images\cloud.png", "Rain":r"images\rain.png", "Snow":r"images\snow.png"}
+            sky = [sky["weather"][0]["main"] for sky in filtered_data]
+            image_paths = [images[condition] for condition in sky]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.info("Invalid place name.")
